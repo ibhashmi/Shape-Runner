dc3951e94b3d47f1c22559063a3fb2a631d0d2cf
Ibrahim <ibhashmi@gmail.com>
2019-01-01 03:31:45 -0500

    Player Score
    Update player score per meter

---
+++ Assets/PlayerScore.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;   //allows to write code for UI
+
+public class PlayerScore : MonoBehaviour
+{
+    public Transform player;    //Transform deals with positions and rotations
+    public Text score;
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = player.position.z.ToString("0");       //add zero to display score without decimals
+    }
+}
