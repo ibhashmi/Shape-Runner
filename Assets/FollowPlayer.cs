83253c7263346e4c28953cf80a12fcb8d98e7a7a
Ibrahim <ibhashmi@gmail.com>
2018-12-31 22:54:16 -0500

    Player Camera Angle
    Set camera angle to follow player shape, providing user with third person view

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 camera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + camera;
+    }
+}
