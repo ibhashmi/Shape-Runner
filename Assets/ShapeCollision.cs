af4f558ba08cf2cdd69c261b9d5d0ed213c5a145
Ibrahim <ibhashmi@gmail.com>
2018-12-31 22:55:32 -0500

    Shape Collisions
    Set game to stop upon first collision between player shape and obstacle

---
+++ Assets/ShapeCollision.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class ShapeCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    private void OnCollisionEnter(Collision collisionInfo) //this code runs 
+    {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("We hit an " + collisionInfo.collider.name + "!");
+            movement.enabled = false;
+        }
+        
+    }
+
+}
