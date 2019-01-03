98bd3e0a30f64b3059bef87717fa0c991bfc55cc
Ibrahim <ibhashmi@gmail.com>
2019-01-02 22:37:32 -0500

    Game Over message
    Script for printing Game Over message in the CONSOLE

---
+++ Assets/GameManager.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;      //used for switching or reloading scenes
+
+public class GameManager : MonoBehaviour
+{
+    private bool gameEnded = false;
+
+    public void GameOver()  //needs to be accessed in PlayerCollision
+    {
+        if (gameEnded == false)
+        {
+            gameEnded = true;
+            Debug.Log("GAME OVER!");
+            RestartGame();
+            
+        }
+        
+    }
+    
+    private void RestartGame()
+    {
+
+    }
+}
